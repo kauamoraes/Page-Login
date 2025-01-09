434b708d1cbe451d0921c0738e9cf946fc76300a
kauamoraes <154555951+kauamoraes@users.noreply.github.com>
2025-01-09 13:32:08 -0300

    style.css

---
+++ style.css
@@ -0,0 +1,100 @@
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Hind+Mysuru:wght@300;400;500;600;700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Palanquin+Dark:wght@400;500;600;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Righteous&family=Sarala:wght@400;700&family=Smooch+Sans:wght@100..900&family=Teko:wght@300..700&display=swap');
+
+*{
+    margin: 0%;
+    padding: 0%;
+    box-sizing: border-box;
+}
+
+body{
+    background-color: black;
+}
+
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 95rem;
+}
+
+.fundo-branco{
+    background-color: white;
+    margin-top: 1.5rem;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 25px;
+}
+
+.Welcome{
+    display: flex;
+    flex-direction: column;
+    font-size: 1.4rem;
+}
+
+.Welcome > p{
+    font-size: 1rem;
+}
+
+main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.Login{
+    display: flex;
+    flex-direction: column;
+    padding: 8rem;
+}
+
+#user-name{
+    height: 45px;
+    width: 18rem;
+    border-radius: 5px;
+    border-style: none;
+    background: rgba(128, 128, 128, 0.253);
+    padding: 0.5rem;
+}
+
+#password{
+    height: 45px;
+    width: 18rem;
+    border-radius: 5px;
+    border-style: none;
+    background: rgba(128, 128, 128, 0.253);
+    padding: 0.5rem;
+}
+
+.Next{
+    height: 35px;
+    border-radius: 25px;
+    background: #1c1c1c;
+    color: white;
+    border-style: none;
+    cursor: pointer;
+}
+
+.Next:hover{
+    background: #1c1c1cdd;
+}
+
+.span-2{
+    display: flex;
+    justify-content: center;
+}
+
+.button-icons{
+    height: 3rem;
+    border-radius: 5px;
+    border: 1px solid black;
+    background: transparent;
+    cursor: pointer;
+}
+
+img{
+    height: 35rem;
+    width: 35rem;
+    padding: 2rem;
+}
